abea57da6be108f57da7fdce94fd797289beb70a
Vlad <MrRefactoring@yandex.ru>
2018-05-13 16:29:12 +0300

    Try fix

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 $(document).ready(() => {
 
+    let images = $('.images-container');
+
+    $(document).scroll(function () {
+        let scrolled = window.pageYOffset || document.documentElement.scrollTop;
+        images.css('top', scrolled);
+    });
+
 });
