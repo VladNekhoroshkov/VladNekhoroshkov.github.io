b33ad881335a0c911116cbb5a6cc9b7a90b65b98
Unknown <ars_pro@bk.ru>
2018-05-14 13:05:24 +0300

    help

--- js/index.js
+++ js/index.js
@@ -1,10 +1,3 @@
 $(document).ready(() => {
 
-    let images = $('.images-container');
-
-    $(document).scroll(function () {
-        let scrolled = window.pageYOffset || document.documentElement.scrollTop;
-        images.css('top', scrolled);
-    });
-
 });
