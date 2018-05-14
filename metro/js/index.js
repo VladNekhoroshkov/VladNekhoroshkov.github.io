453db118eb7f0d2945f28202583e5a3771e2a109
Vlad <MrRefactoring@yandex.ru>
2018-05-14 13:02:41 +0300

    Fix margin

---
+++ metro/js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+
+    let wraper = $('#main-slider');
+    let sample = $('#sample-image');
+
+    window.onresize = () => {
+        wraper.css('height', sample.height());
+    };
+
+    wraper.css('height', sample.height());
+
+});
